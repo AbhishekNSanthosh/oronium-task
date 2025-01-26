e413d78cea0b2986a729eb727e9d2f79f7002099
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-26 21:20:37 +0530

    feat:header

--- src/common/components/Toggle.tsx
+++ src/common/components/Toggle.tsx
@@ -1,30 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { MdSunny } from "react-icons/md";
-import { IoMoon } from "react-icons/io5";
+import React, { useState } from "react";
 
-const DarkModeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") || "light"
-  ); // Initialize with "light" if no theme is set in localStorage
+const Toggle = () => {
+  const [lang, setLang] = useState("EN"); // Default language is English
 
-  // Effect to apply dark mode based on initial theme setting
-  useEffect(() => {
-    const body = document.body;
-    if (theme === "dark") {
-      body.classList.add("dark");
-    } else {
-      body.classList.remove("dark");
-    }
-  }, [theme]);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    const newTheme = isDarkMode ? "light" : "dark"; // Toggle between light and dark mode
-    localStorage.setItem("theme", newTheme); // Update localStorage with new theme
-    setTheme(newTheme); // Update state with new theme
+  const toggleLang = () => {
+    setLang((prevLang) => (prevLang === "EN" ? "IN" : "EN")); // Toggle between "EN" and "IN"
   };
 
   return (
@@ -33,31 +15,35 @@ const DarkModeToggle = () => {
         <input
           type="checkbox"
           className="sr-only"
-          checked={theme === "dark"}
-          onChange={toggleDarkMode}
+          checked={lang === "IN"}
+          onChange={toggleLang}
         />
-        <div className="w-20 h-10 bg-breakerDay-200 rounded-full dark:bg-gray-700 relative">
+        <div className="w-24 h-10 bg-gray-200 rounded-full relative">
+          {/* Sliding Toggle */}
           <div
-            className={`absolute top-[1.8px] left-0.5 w-9 h-9 rounded-full shadow transform transition-transform duration-300 flex items-center justify-center ${
-              theme === "dark" ? "translate-x-10 bg-gray-600" : "bg-breakerDay-100"
-            }`}
+            className={`absolute top-[1.8px] ${
+              lang === "IN" ? "translate-x-[calc(100%-2px)]" : "translate-x-0"
+            } left-0.5 w-12 h-9 rounded-full shadow bg-white transform transition-transform duration-300 flex items-center justify-center`}
           ></div>
-          <IoMoon
-            className={`absolute right-[8px] top-[8.5px] ${
-              theme === "dark" ? "text-breakerDay-600 " : "text-gray-100"
-            }`}
-            size={24}
-          />
-          <MdSunny
-            className={`absolute left-[7px] top-[9px] ${
-              theme === "dark" ? "text-white" : "text-orange-500"
-            } dark:text-gray-300`}
-            size={24}
-          />
+          {/* EN and IN labels */}
+          <span
+            className={`absolute left-[15px] top-[9.5px] ${
+              lang === "EN" ? "text-gray-900" : ""
+            } font-medium`}
+          >
+            EN
+          </span>
+          <span
+            className={`absolute right-4 top-2.5 ${
+              lang === "IN" ? "text-gray-900" : ""
+            } font-medium`}
+          >
+            IN
+          </span>
         </div>
       </label>
     </div>
   );
 };
 
-export default DarkModeToggle;
+export default Toggle;
--- src/widgets/Header/view.tsx
+++ src/widgets/Header/view.tsx
@@ -1,4 +1,9 @@
+import Toggle from "@components/Toggle";
 import React from "react";
+import { BsBookmarkDash } from "react-icons/bs";
+import { IoCartOutline } from "react-icons/io5";
+import { HiOutlineMenu } from "react-icons/hi";
+import Image from "next/image";
 
 export default function Header() {
   return (
@@ -13,7 +18,35 @@ export default function Header() {
             <span className="">London</span>
           </div>
         </div>
-        <div className="flex-[1]"></div>
+        <div className="flex-[1] flex flex-row items-center gap-[60px] justify-end">
+          <Toggle />
+          <div className="flex flex-row items-center gap-[30px] cursor-pointer">
+            <div className="relative">
+              <div className="absolute bg-[#F9CF26] rounded-full text-[10px] font-semibold w-[20px] items-center justify-center h-[20px] bottom-[17px] right-[-10px] flex">
+                21
+              </div>
+              <BsBookmarkDash className="text-[21px] cursor-pointer" />
+            </div>
+            <div className="relative">
+              <div className="absolute bg-[#F9CF26] rounded-full text-[10px] font-semibold w-[20px] items-center justify-center h-[20px] bottom-[17px] right-[-10px] flex">
+                <span className="mt-1">1</span>
+              </div>
+              <IoCartOutline className="text-[25px] cursor-pointer" />
+            </div>
+          </div>
+          <div className="flex flex-row items-center justify-between gap-5 border-2 border-gray-500 pl-4 pr-1 rounded-full py-1">
+            <HiOutlineMenu className="text-[20px] cursor-pointer" />
+            <div className="bg-gray-300 rounded-full">
+              <Image
+                src={"/messi.png"}
+                height={300}
+                width={300}
+                alt=""
+                className="w-[34px] h-[34px] rounded-full"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
