d2b4caba90f0f6216ff37baa48d70761f0574d57
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2025-01-27 15:55:52 +0530

    feat: responsive

--- src/widgets/Footer/view.tsx
+++ src/widgets/Footer/view.tsx
@@ -1,53 +1,56 @@
 import ScrollToTopButton from "@components/ScrollToTop";
 import Image from "next/image";
 import React from "react";
-import { IoMdArrowRoundUp } from "react-icons/io";
 import { IoMdSend } from "react-icons/io";
 
 export default function Footer() {
   return (
-    <div className="px-[30px] h-[530px] w-full flex-row flex pt-[100px] relative">
-      <div className="flex-1 h-full">
-        <span className="font-medium">
+    <div className="px-6 lg:px-12 py-8 lg:py-16 w-full flex flex-col lg:flex-row relative pb-[200px] lg:pb-[300px]">
+      {/* Left Section */}
+      <div className="flex-1 mb-8 lg:mb-0 lg:pr-8">
+        <span className="font-medium text-base lg:text-lg leading-relaxed">
           We offer adult clinics that <br /> focus on skill improvement
         </span>
-        <div className="absolute bottom-[50px] left-[30px] bg-[#F0F0F0] w-[390px] flex flex-row items-center rounded-full pl-2 pr-1 py-1">
+        <div className="lg:absolute lg:bottom-[80px] mt-6 lg:mt-10 bg-gray-200 w-full max-w-md flex items-center rounded-full pl-4 pr-2 py-2">
           <input
             type="text"
-            className="w-full bg-[#F0F0F0] px-2 py-1 outline-none border-none placeholder:text-gray-800"
+            className="flex-1 bg-transparent px-2 bg-gray-200 py-1 text-sm lg:text-base outline-none placeholder:text-gray-600"
             placeholder="youremail@gmail.com"
           />
-          <div className="bg-[#200F33] p-2 rounded-full flex items-center justify-center">
-            <IoMdSend className="text-[30px] text-white" />
+          <div className="bg-[#200F33] p-3 rounded-full flex items-center justify-center cursor-pointer">
+            <IoMdSend className="text-xl lg:text-2xl text-white" />
           </div>
         </div>
       </div>
-      <div className="flex-1 flex flex-row space-x-[100px] relative">
-        <div className="flex flex-col gap-2">
-          <span className="text-gray-700 font-medium">Home</span>
-          <span className="text-gray-700 font-medium">Service</span>
-          <span className="text-gray-700 font-medium">About</span>
-          <span className="text-gray-700 font-medium">Product</span>
+
+      {/* Middle Section */}
+      <div className="flex-1 flex flex-wrap gap-y-6 lg:gap-y-0 lg:gap-x-16 relative">
+        <div className="flex flex-col gap-2 w-1/2 lg:w-auto">
+          <span className="text-gray-700 font-medium text-sm lg:text-base">Home</span>
+          <span className="text-gray-700 font-medium text-sm lg:text-base">Service</span>
+          <span className="text-gray-700 font-medium text-sm lg:text-base">About</span>
+          <span className="text-gray-700 font-medium text-sm lg:text-base">Product</span>
         </div>
-        <div className="flex flex-col gap-2">
-          <span className="text-gray-700 font-medium">Home</span>
-          <span className="text-gray-700 font-medium">Service</span>
-          <span className="text-gray-700 font-medium">About</span>
-          <span className="text-gray-700 font-medium">Product</span>
+        <div className="flex flex-col gap-2 w-1/2 lg:w-auto">
+          <span className="text-gray-700 font-medium text-sm lg:text-base">Home</span>
+          <span className="text-gray-700 font-medium text-sm lg:text-base">Service</span>
+          <span className="text-gray-700 font-medium text-sm lg:text-base">About</span>
+          <span className="text-gray-700 font-medium text-sm lg:text-base">Product</span>
         </div>
 
         {/* Scroll to Top Button */}
-        <div className="absolute right-[40px] top-[0px">
+        <div className="absolute right-[-2.5rem] bottom-[-30px] lg:top-0 lg:right-10">
           <ScrollToTopButton />
         </div>
       </div>
 
+      {/* Background Image */}
       <Image
-        src={"/trvvr.svg"}
-        alt=""
+        src="/trvvr.svg"
+        alt="Background Illustration"
         width={1000}
         height={1000}
-        className="w-[60rem] absolute bottom-0 right-0"
+        className="w-[40rem] lg:w-[60rem] absolute bottom-0 right-0 opacity-80"
       />
     </div>
   );
